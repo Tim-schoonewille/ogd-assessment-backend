37c4efe996f893f6e5f5b1a3a9486fd05ade753d
n0ra <tim.schoonewille@gmail.com>
2024-03-08 20:05:45 +0100

    create cache provider with tests

---
+++ app/redis.py
@@ -0,0 +1,18 @@
+from redis import asyncio as aioredis
+from redis.asyncio import Redis as AsyncRedis
+
+from app.config import get_config
+
+
+config = get_config()
+
+# REDIS_URL = f'redis://:{config.REDIS_PASSWORD}@{config.REDIS_HOST}/0:6379'
+
+
+# async def get_cache():
+#     """Dependency for FastAPI endpoints."""
+#     redis = aioredis.from_url(url=REDIS_URL)
+#     async with redis.client() as client:
+#         yield client
+
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,37 @@
 from httpx import AsyncClient
 import pytest_asyncio
+from redis import asyncio as aioredis
 
+from app.config import get_config
 from app.main import init_fastapi
 
 API_BASE_URL = 'http://localhost:8000/api/v1'
 
 
+# @pytest_asyncio.fixture(scope='function')
+# async def std_fastapi_client():
+#     app = init_fastapi(testing=True)
+
+#     async with AsyncClient(app=app, base_url=API_BASE_URL, follow_redirects=True) as c:
+#         yield c
+
+
+# @pytest_asyncio.fixture(scope='function')
+# async def fastapi_trailer():
+#     """Fastapi fixture for trailer service."""
+#     app = init_fastapi(testing=True)
+
+#     async with AsyncClient(app=app, base_url=API_BASE_URL, follow_redirects=True) as c:
+#         yield c
+
+
 @pytest_asyncio.fixture(scope='function')
-async def std_fastapi_client():
-    app = init_fastapi(testing=True)
-
-    async with AsyncClient(app=app, base_url=API_BASE_URL, follow_redirects=True) as c:
+async def cache():
+    """Fixture to receive async redis cache client. Flushes the DB on every call."""
+    config = get_config()
+    url = 'redis://:REDIS_PASSWORD@cache/0:6379'
+    redis = aioredis.from_url(url)
+    await redis.execute_command('FLUSHALL ASYNC')
+    async with redis.client() as c:
         yield c
-
-
-@pytest_asyncio.fixture(scope='function')
-async def fastapi_trailer():
-    """Fastapi fixture for trailer service."""
-    app = init_fastapi(testing=True)
-
-    async with AsyncClient(app=app, base_url=API_BASE_URL, follow_redirects=True) as c:
-        yield c
+        await c.aclose()
