c981b8287d9855979b5e906e64048993bfba73bd
n0ra <tim.schoonewille@gmail.com>
2024-03-08 18:39:49 +0100

    Update conftest with std fastapi client for endpoint testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,14 @@ API_BASE_URL = 'http://localhost:8000/api/v1'
 
 
 @pytest_asyncio.fixture(scope='function')
+async def std_fastapi_client():
+    app = init_fastapi(testing=True)
+
+    async with AsyncClient(app=app, base_url=API_BASE_URL, follow_redirects=True) as c:
+        yield c
+
+
+@pytest_asyncio.fixture(scope='function')
 async def fastapi_trailer():
     """Fastapi fixture for trailer service."""
     app = init_fastapi(testing=True)
