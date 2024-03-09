1bca0c70ce2877c418629668eaadc474bf345c66
n0ra <tim.schoonewille@gmail.com>
2024-03-09 21:53:29 +0100

    Created mock trailer service for endpoint testing with tests

--- tests/data/trailer/mock_utilities.py
+++ tests/data/trailer/mock_utilities.py
@@ -1,6 +1,9 @@
+import asyncio
+import json
 from app import models
 from app.config import ConfigBase
 from app.interfaces import ICacheProvider
+from app.trailer.exceptions import InvalidIMDBId, MovieNotFoundError
 from app.trailer.interfaces import IMovieDataProvider, ITrailerProvider, ITrailerService
 from app.trailer.models import (
     CompactMovieData,
@@ -30,19 +33,67 @@ class MockTrailerService(ITrailerService):
 
     @property
     def compact_movie_data_entries(self) -> dict[str, list[models.CompactMovieData]]:
-        pass
+        compact_movies = {}
+        json_files = [
+            ('star wars', './tests/data/trailer/search-star-wars2.json'),
+            ('lord of the rings', './tests/data/trailer/search-lotr.json'),
+            ('indiana jones', './tests/data/trailer/search-indiana-jones.json'),
+        ]
+        for movie in json_files:
+            with open(movie[1], 'r', encoding='utf-8') as f:
+                data = json.loads(f.read())
+            compact_movies.update(
+                {movie[0]: [models.CompactMovieData(**movie_data) for movie_data in data]}
+            )
+        return compact_movies
 
     @property
     def movie_data_with_trailer_entries(self) -> dict[str, models.MovieDataWithTrailer]:
-        pass
+        movie_data_with_trailer = {}
+        sources = [
+            './tests/data/trailer/trailer-result-starwars.json',
+            './tests/data/trailer/trailer-result-lotr.json',
+            './tests/data/trailer/trailer-result-indiana-jones.json',
+        ]
+        for source in sources:
+            with open(source, 'r', encoding='utf-8') as f:
+                data = json.loads(f.read())
 
-    async def search(self, query: str) -> TrailerResult:
-        return None
+            for movie_with_trailer in data['movies']:
+                movie_data_with_trailer.update(
+                    {
+                        movie_with_trailer['imdbID']: models.MovieDataWithTrailer(
+                            **movie_with_trailer
+                        )
+                    }
+                )
+        return movie_data_with_trailer
+
+    async def search(self, query: str, network_lag: float = 1.5) -> TrailerResult:
+        await asyncio.sleep(network_lag)
+        compact_movie_list = self.compact_movie_data_entries[query]
+        movies_with_trailer = [
+            self.movie_data_with_trailer_entries[movie.imdbID]
+            for movie in compact_movie_list
+        ]
+        return TrailerResult(movies=movies_with_trailer)
 
     async def get_movie_data_with_trailer_by_imdb_id(
-        self, _id: str, title: str
+        self, _id: str, title: str, network_lag: float = 1.5
     ) -> MovieDataWithTrailer:
-        return None
+        await asyncio.sleep(network_lag)
+        try:
+            movie_data_with_trailer = self.movie_data_with_trailer_entries[_id]
+        except KeyError:
+            raise InvalidIMDBId('INVALID_IMDB_ID')
+        return movie_data_with_trailer
 
-    async def get_compact_movie_data_by_query(self, query: str) -> list[CompactMovieData]:
-        return None
+    async def get_compact_movie_data_by_query(
+        self, query: str, network_lag: float = 1.5
+    ) -> list[CompactMovieData]:
+        await asyncio.sleep(network_lag)
+        try:
+            compact_movie_data = self.compact_movie_data_entries[query.strip()]
+        except KeyError:
+            raise MovieNotFoundError('MOVIE_NOT_FOUND')
+        return compact_movie_data
